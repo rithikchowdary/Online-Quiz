e0b2b77422df466a246a9ef356ab2b325a067c7c
rithik <38383049+rithikchowdary@users.noreply.github.com>
2020-04-12 13:22:06 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>| Online Quiz System |</title>
+                <style type="text/css">
+            body {
+                width: 100%;
+                background:gray ;
+                background-position: center center;
+                background-repeat: repeat;
+                background-attachment: all;
+                background-size: cover;
+            }
+        </style>
+    </head>
+    <body>
+        <center>
+            <div class="intro">
+                
+                <h1  > Online quiz system </h1>
+                <a href="login.php" class="btn"> login </a> &emsp;
+                <a href="register.php" class="btn"> register </a>
+                <h2> Good &nbsp;Luck. </h2>
+            </div>
+        </center>
+    </body>
+</html>
